3bd748c1493a3fce3395fa2655067d4725f7fa5e
Nicol Vojacek <nvojacek@gmail.com>
2020-05-17 18:56:23 +0200

    Req to weather api going through

--- api-ts/src/index.ts
+++ api-ts/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import request from 'request'
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
 
-app.get("/test", function (req, res) {
-  res.send("Hello World");
+app.get('/test', function(req, res: any) {
+  console.log(req)
+  request({
+    uri: 'https://fcc-weather-api.glitch.me/api/current',
+    qs: {
+      lat: req.query.lat,
+      lon: req.query.lon
+    }
+
+  }).pipe(res);
 });
 
 app.listen(9000);
--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,24 +1,41 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-const callAPI = async () => {
-  const response = await fetch("http://localhost:9000/test");
-  console.log({ response });
+const callAPI = async (coordinates: any) => {
+  console.log(coordinates)
+  const response = await fetch(
+    `http://localhost:9000/test?lat=${coordinates.lat}&lon=${coordinates.lon}`
+  );
   return response.text();
 };
 
 function App() {
   const [apiResponse, setApiResponse] = useState("");
 
-  useEffect(() => {
-    callAPI().then((result) => setApiResponse(result));
-  }, []);
-
   return (
     <div className="App">
       <p>{apiResponse}</p>
+
+      <p>Click the button to get your coordinates.</p>
+
+      <button
+        onClick={async () => {
+          console.log(2)
+          const position: any = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition((position) =>
+              resolve(position)
+            );
+          });
+          console.log(1, position)
+          callAPI({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          }).then((result) => setApiResponse(result));
+        }}
+      >
+        Try It
+      </button>
     </div>
   );
 }
-
 export default App;
